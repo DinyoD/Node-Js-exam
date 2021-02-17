a7c578b0513aa72acc9af88b96b532c190244584
DinyoD <40523718+DinyoD@users.noreply.github.com>
2021-02-17 20:41:06 +0200

    Add public(static) folder, views with layouts and partials, and some test contents.

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const env = process.env.NODE_ENV || 'dev';
 const config = require('./config')[env];
 const setupExpress = require('./config/express')
 
-
+const content = [
+    {
+        title: 'Title 1',
+        number: 1,
+    },
+    {
+        title: 'Title 12',
+        number: 12,
+    },
+    {
+        title: 'Title 123',
+        number: 123,
+    },
+]
 const app = express();
 setupExpress(app);
+
+app.get('/', (req, res) => {
+    res.render('home', { content });
+})
 
 //TODO routes, mongoose, error handler
 
